83cd970676558a634ed8ce919efbb6142b851fb3
hoxsey22 <joe.hoxsey.60@my.csun.edu>
2014-10-21 10:58:05 -0700

    Changed everything

--- app/src/main/java/com/example/joseph/maps/MainActivity.java
+++ app/src/main/java/com/example/joseph/maps/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.joseph.maps;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -45,21 +52,33 @@ public class MainActivity extends Activity {
     private PolylineOptions polylineOptions;
     private List<LatLng> points;
     private TextView locationTextView;
-    public Calendar calendar;
+    private TextView speedTextView;
     public long currentTime;
     public long lastTime;
+    public double speed;
+    public double GOOD_WALK_SPEED = 3.1;
+    public double DECENT_WALK_SPEED = 2.00;
+    public double SLOW_WALK_SPEED = 1.00;
+    public double MPS_CONVERSION_MPH = 0.44704;
+    public WifiManager wifiManager;
+    public WifiReceiver wifiReceiver;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        locationTextView = (TextView) findViewById(R.id.location);
 
+        locationTextView = (TextView) findViewById(R.id.location);
         mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         mMap.setMyLocationEnabled(true);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        wifiReceiver = new WifiReceiver();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
+        //debugging
+        checkWifi();
 
         gc = new Geocoder(this, Locale.getDefault());
 
@@ -68,12 +87,12 @@ public class MainActivity extends Activity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))	{
+                if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))    {
                     locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
                     location.setProvider(locationManager.GPS_PROVIDER);
                     displayLocation(location);
                 }
-                else if(!locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))	{
+                else if(!locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))    {
                     locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, locationListener);
                     location.setProvider(locationManager.NETWORK_PROVIDER);
                     displayLocation(location);
@@ -96,12 +115,12 @@ public class MainActivity extends Activity {
 
             }
         };
-        if(!locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))	{
+        if(!locationManager.isProviderEnabled(locationManager.GPS_PROVIDER))    {
             locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER, 0, 0, locationListener);
             lastLocation = new Location(locationManager.NETWORK_PROVIDER);
             displayLocation(lastLocation);
         }
-        else	{
+        else    {
             locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
             lastLocation = new Location(locationManager.GPS_PROVIDER);
             displayLocation(lastLocation);
@@ -117,44 +136,44 @@ public class MainActivity extends Activity {
                 if (checked)    {
                     mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 }
-                    break;
+                break;
             case R.id.satellite_map:
                 if (checked)    {
                     mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
                 }
-                    break;
+                break;
             case R.id.hybrid_map:
                 if (checked)    {
                     mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
                 }
-                    break;
+                break;
         }
     }
-    public void displayLocation(Location location)	{
+    public void displayLocation(Location location)    {
 
         locationTextView.setText("Latitude: " + location.getLatitude()
-                        + " Longitude: " + location.getLongitude());
+                + " Longitude: " + location.getLongitude());
         if(location.getLongitude() != 0 && location.getLatitude() != 0 && lastLatitude !=0 && lastLongitude != 0) {
             LatLng mapCenter = new LatLng(location.getLatitude(), location.getLongitude());
 
             lastTime = currentTime;
-            currentTime = calendar.getTimeInMillis();
-            findSpeed(lastLatitude, lastLongitude, location.getLatitude(), location.getLongitude(),lastTime,currentTime);
+            currentTime = location.getTime();
+
+            findSpeed(location.getLatitude(),location.getLongitude(), lastLatitude, lastLongitude,lastTime,currentTime);
+          //  speedTextView.setText("Speed: "+speed);
 
             polylineOptions = new PolylineOptions().geodesic(true)
                     .add(new LatLng(location.getLatitude(), location.getLongitude()), new LatLng(lastLatitude, lastLongitude))
                     .width(25)
-                    .color(Color.WHITE);
+                    .color(setPolyLineColor(speed));
 
 
             mMap.addPolyline(polylineOptions);
-
-
         }   else    {
             LatLng mapCenter = new LatLng(location.getLatitude(), location.getLongitude());
 
-            currentTime = calendar.getTimeInMillis();
-            lastTime = calendar.getTimeInMillis();
+            currentTime = location.getTime();
+            lastTime = location.getTime();
 
             polylineOptions = new PolylineOptions().geodesic(true)
                     .add(new LatLng(location.getLatitude(), location.getLongitude()))
@@ -167,6 +186,22 @@ public class MainActivity extends Activity {
         lastLatitude = location.getLatitude();
         lastLongitude = location.getLongitude();
     }
+    /*
+        Function: Finds the distance between the two points based on Lat and Long
+        Parameters:
+            double first latitude, first longitude, second latitude, second longitude
+        Functionality:
+            1. Converts the lats and longs to Radians
+            2. Set r to the radius of the earth
+            3. Finds the vertices of the two points P and Q
+            4. Finds the Dot Product
+            5. Find the new found distances in Meters
+         Returns:
+            double distance between two points
+         Additional Information:
+            This code/equation can be found:
+                http://www.ridgesolutions.ie/index.php/2013/11/14/algorithm-to-calculate-speed-from-two-gps-latitude-and-longitude-points-and-time-difference/
+     */
     public double findDistance(double lat1, double lon1, double lat2, double lon2)  {
 
         lat1 = lat1 * Math.PI / 180.00;
@@ -197,12 +232,65 @@ public class MainActivity extends Activity {
         // Distance in Metres
         return r * theta;
     }
-    public double findSpeed(double lat1, double lon1, double lat2, double lon2, long time1, long time2)   {
+    /*
+        Function: Find thee user speed in Mile Per Hour
+        Parameters:
+            double first latitude, first longitude, second latitude, second longitude,
+                first "timestamp", second "timestamp"
+        Functionality:
+            1. Finds the difference between the two distances(lat and long)
+            2. Find the difference between the two times
+            3. Find the speed based on meters per second
+            4. *Optional* finds the kilometer per hour
+            5. Sets the global "speed" by the conversion into mile per hour
+         Returns:
+            VOID
+     */
+    public void findSpeed(double lat1, double lon1, double lat2, double lon2, long time1, long time2)   {
         double distance = findDistance(lat1,lon1,lat2,lon2);
-        double time = (time2 - time1)/1000.0;
-        double speedMPH = distance/time;
-        double speedKPH = (speedMPH*3600.0)/1000.0;
-        return speedMPH;
+        //find the difference between the two times found
+        double time = (time2 - time1)/1000.00;
+        // divides distance and the time previously found in order
+
+        double speedMPS = distance/time; //meters per second
+        double speedKPH = (speedMPS*3600.00)/1000.00;
+        speed = speedMPS/MPS_CONVERSION_MPH;
+    }
+    /*
+        Function: Set the polyline color based on the speed of the user
+        Parameters:
+            double s which is the speed
+        Functionality:
+            1. If: speed is less than 2.0 (2 MPH) then the polyline will stay RED
+            2. Else if: The speed is in between 2 and 3 then the polyline will return YELLOW
+            3. Else: Return GREEN meaning the user is going a good speed based on Human average walking speed
+         Returns:
+            int Color
+         Additional Information:
+            The average walking speed of a human is 3.1 MPH this is based on:
+                "the average human walking speed is about 5.0 kilometres per hour (km/h), or about 3.1 miles per hour (mph)"
+                http://en.wikipedia.org/wiki/Walking
+     */
+    public int setPolyLineColor(double s)  {
+        if(s < DECENT_WALK_SPEED)
+            return Color.RED;
+        else if(s > DECENT_WALK_SPEED && s < GOOD_WALK_SPEED)
+            return Color.YELLOW;
+        else
+            return Color.GREEN;
+    }
+
+
+    public void checkWifi()  {
+        List<ScanResult> scanResults = wifiManager.getScanResults();
+        for(int i= 0; i < scanResults.size(); i++)  {
+            Log.i("WIFI state:",scanResults.get(i).toString());
+        }
+    }
+
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+        }
     }
 
 
